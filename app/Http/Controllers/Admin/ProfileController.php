40394628d870c8b52dfc804eecfbc4a9ae147945
”名前” <>
2019-12-13 14:15:37 +0000

    first commit

---
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Profile;
+use App\ProfileHistory;
+use Carbon\Carbon;
+
+
+class ProfileController extends Controller
+{
+    //
+    public function add()
+    {
+        return view('admin.profile.create');
+    }
+
+    public function create(Request $request)
+    {
+        $this->validate($request, Profile::$rules);
+
+        $profiles = new Profile;
+        $form = $request->all();
+        
+        unset($form['_token']);
+        
+        $profiles->fill($form);
+        $profiles->save();
+        return redirect('admin/profile/create');
+    }
+
+    public function edit(Request $request)
+    {   
+        $profiles = Profile::find($request->id);
+        if (empty($profiles)) {
+        abort(404);    
+        }
+        return view('admin.profile.edit', ['profiles_form' => $profiles]);
+    }
+
+    public function update(Request $request)
+    {
+        $this->validate($request, Profile::$rules);
+
+        $profiles = Profile::find($request->id);
+        $profiles_form = $request->all();
+        
+        unset($profiles_form['_token']);
+
+        $profiles->fill($profiles_form)->save();
+        
+        $history = new ProfileHistory;
+        $history->profile_id = $profiles->id;
+        $history->edited_at = Carbon::now();
+        $history->save();
+        return redirect('admin/profile/create');
+    }
+}
